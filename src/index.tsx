9b7b489a84b96e26e26cd28e682f54e57354752f
mochi256 <mochi256@outlook.com>
2020-11-21 10:18:22 +0900

    update: start test

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './components/App'
 
 ReactDOM.render(
-  <div>
-    test
-  </div>,
+  <App />,
   document.getElementById('root')
 );
 
